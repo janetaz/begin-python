955ead79ca78e5bde07f1372601bc9bedf4c2cf0
janetaz <30960556+janetaz@users.noreply.github.com>
2018-02-01 22:57:34 -0700

    Add files via upload
    Just able to click 'Next!' Now, going to add game loop, with stop exception

--- twig.py
+++ twig.py
@@ -18,8 +18,23 @@ for p in divtag:
             count += 1
 print (count)
 
+
+#_____Browser Interaction______
 browse = webdriver.Firefox(executable_path = '/Users/tkhamsi/Desktop/Projects/geckodriver')
-browse.get()
+browse.get(url)
+touch = browse.find_element_by_link_text('Next')
+touch.click()
+
+#________Finding 'Next' with bs_________
+#next1 = divtag.find('a')
+#nexttext = next1.string
+#if nexttext == 'Next':
+#    print (nexttext)
+#___________________________
+#can I do a bsoup search find_element that's 'an a tag containing 'next'?
+#I'll try 
+#oh! I need to find >Next<
+    
 #ok, this OPENS the window. we don't need that; we need...wait we do
 #we need to open it; then click 'next': then get THAT url 
 #so some kind of updating while loop
